050f574e7b2251513590425c6fa01ce8e41dff43
boris-ghomar <boris@ghomar.com>
2024-10-14 18:25:27 +0400

    change api-docs route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", function () {
-    return redirect("/dashboards/home");
-});
+Route::redirect("/", "/dashboards/home");
 
-Route::get('api-docs', function () {
-    return view('redoc');
-});
+Route::view("api-docs/v1", "redoc");
